842faab039ed9acf8b9cbb5bffc0f9cd6318e548
Skry Blackfall <js0877658@otc.edu>
2017-10-31 21:53:18 -0500

    added chalk stuff

--- index.js
+++ index.js
@@ -1,1 +1,7 @@
-console.log("hello world");
+const chalk = require('chalk');
+console.log(chalk.blue('I once knew a man by the name of Chad'));
+console.log(chalk.green('Chad was pretty bad'))
+console.log(chalk.bold.red('But Chad was also pretty rad'))
+console.log(chalk.black('No one knew that Chad was actually very sad'))
+console.log(chalk.bold.yellow('Stay in school kids'))
+console.log(chalk.underline.green('Or you may end up like bad, rad, sad, Chad.'))
